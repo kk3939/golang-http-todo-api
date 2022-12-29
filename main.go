b1c1580e0161d6c127c3649c9791f4f2a767a4c9
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-29 19:35:42 +0900

    add todo get json

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Todo struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type Todos []Todo
+
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{}
+	for i := 1; i <= 10; i++ {
+		todos = append(todos, Todo{Id: i, Name: fmt.Sprintf("name_%d", i), Content: fmt.Sprintf("content_%d", i)})
+	}
+	json.NewEncoder(w).Encode(todos)
+}
+
 func main() {
-	fmt.Println("Hellow World!")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "/ access is success!")
-		fmt.Println("logged.")
-	})
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	fmt.Println("Start....")
+	http.HandleFunc("/todo", getTodo)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
