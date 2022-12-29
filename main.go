d2e8ec0f0cc3bcfc347ba7677be35ad72ac2678d
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-29 19:04:04 +0900

    add handler

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hellow World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "/ access is success!")
+		fmt.Println("logged.")
+	})
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
