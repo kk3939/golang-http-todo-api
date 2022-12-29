9dfccdd0c04194c1ab85fa5f68df4c1ec4c40a32
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-29 22:12:57 +0900

    add seeds function

--- main.go
+++ main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type Todo struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Content string `json:"content"`
+	Id      int `gorm:"primary_key"`
+	Name    string
+	Content string
 }
 
-type Todos []Todo
+var Db *gorm.DB
 
-func getTodo(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{}
-	for i := 1; i <= 10; i++ {
-		todos = append(todos, Todo{Id: i, Name: fmt.Sprintf("name_%d", i), Content: fmt.Sprintf("content_%d", i)})
+func connect(count int) {
+	dsn := "test:password@tcp(golang-http-todo-api-db)/todo?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		if count > 1 {
+			time.Sleep(time.Second * 2)
+			count--
+			fmt.Printf("retry... count:%v\n", count)
+			connect(count)
+			return
+		}
+		panic(err)
 	}
-	json.NewEncoder(w).Encode(todos)
 }
 
 func main() {
-	fmt.Println("Start....")
-	http.HandleFunc("/todo", getTodo)
+	var count = 30
+	connect(count)
+
+	if t := Db.Migrator().HasTable(&Todo{}); t {
+		fmt.Println("already Todo table exist. Seeds does not create table.")
+
+	} else if err := Db.Migrator().CreateTable(&Todo{}); err != nil {
+		panic(err)
+	}
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
