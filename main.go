b153b3ee506a2db1e1c6565a754bc0e156ffb6a1
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-29 22:51:13 +0900

    add get method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,6 +16,8 @@ type Todo struct {
 	Name    string
 	Content string
 }
+
+type ToDos []Todo
 
 var Db *gorm.DB
 
@@ -34,15 +37,37 @@ func connect(count int) {
 	}
 }
 
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	var todos ToDos
+	result := Db.Find(&todos)
+	if err := result.Error; err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	json.NewEncoder(w).Encode(&todos)
+}
+
 func main() {
 	var count = 30
 	connect(count)
 
 	if t := Db.Migrator().HasTable(&Todo{}); t {
 		fmt.Println("already Todo table exist. Seeds does not create table.")
-
-	} else if err := Db.Migrator().CreateTable(&Todo{}); err != nil {
-		panic(err)
+	} else {
+		if err := Db.Migrator().CreateTable(&Todo{}); err != nil {
+			panic(err)
+		}
+		var todos ToDos
+		for i := 1; i <= 10; i++ {
+			todos = append(todos, Todo{
+				Id:      i,
+				Name:    fmt.Sprintf("name_%d", i),
+				Content: fmt.Sprintf("content_%d", i),
+			})
+		}
+		Db.Create(&todos)
+		fmt.Println("create ten todo data!")
 	}
+	http.HandleFunc("/todos", getTodos)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
